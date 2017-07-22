ee114a7f4fd3276f70dfcc9e3ea9b8f39d6dd9f8
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-07-22 15:42:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#! /bin/bash
-
+#! /bin/bash                                                                                                                                 
+ 
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BACKUP=${DOTFILES}_backup
 
-# setup symlinks
 for i in $(pushd "$DOTFILES" && git ls-files && popd); do
-	# create backup
-	mkdir --parents "$BACKUP/$i"; mv "$i" $_
-	# create symlink
-	ln -s "$DOTFILES/$i" "$HOME/$i"
+  if [ -f "$HOME/$i" ]; then
+    mkdir --parents "$(dirname "$BACKUP/$i")"; mv "$HOME/$i" $_
+  fi
+  
+  ln -s "$DOTFILES/$i" "$HOME/$i"
 done
