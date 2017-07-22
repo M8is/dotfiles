1c4dc4b21731214f5b6a7eb24fb21f88b98cc006
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-07-22 15:54:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #! /bin/bash                                                                                                                                 
  
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DOTFILES"
+
 BACKUP=${DOTFILES}_backup
 
-for i in $(pushd "$DOTFILES" && git ls-files && popd); do
+git ls-files --exclude-standard | while read i ; do
   if [ -f "$HOME/$i" ]; then
     mkdir --parents "$(dirname "$BACKUP/$i")"; mv "$HOME/$i" $_
   fi
   
-  ln -s "$DOTFILES/$i" "$HOME/$i"
+  mkdir --parents "$(dirname "$HOME/$i")"; ln -s "$DOTFILES/$i" "$HOME/$i"
 done
+
+popd
