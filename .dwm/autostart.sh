9460427c2a84f96ca57bf160607d00108e191e45
Mattis Kaemmerer <mattis.kaemmerer@gmx.de>
2020-01-26 19:49:35 +0100

    Remove conky and tint2

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,17 +6,7 @@ if which compton >/dev/null 2>&1; then
   compton -b
 fi
 
-# start conky system monitor
-if which conky >/dev/null 2>&1; then
-  conky -d
-fi
-
 # restore wallpaper
 if which nitrogen >/dev/null 2>&1; then
   nitrogen --restore &
-fi
-
-# start tint2 
-if which tint2 >/dev/null 2>&1; then
-  tint2 &
 fi
